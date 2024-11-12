cbd5ca30f9f809a5476785da29e0083198a8b23f
Jordi-debug <jordifrikigamer@gmail.com>
2024-11-12 18:46:45 +0100

    .

---
+++ exercici0010.py
@@ -0,0 +1,108 @@
+import math
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+import sys
+import utils1
+import utils
+
+# Definir colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (50, 120, 200)
+GREEN = (0,254,32)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+DARK_GRAY = (64, 64, 64)
+GRAY = (128, 128, 128)
+LIGHT_GRAY = (192, 192, 192)
+SOFT_RED = (244, 67, 54)
+SOFT_GREEN = (76, 175, 80)
+SOFT_BLUE = (33, 150, 243)
+SOFT_YELLOW = (255, 235, 59)
+SOFT_ORANGE = (255, 152, 0)
+SOFT_PURPLE = (156, 39, 176)
+PASTEL_PINK = (255, 182, 193)
+PASTEL_GREEN = (119, 221, 119)
+PASTEL_BLUE = (173, 216, 230)
+PASTEL_PURPLE = (216, 191, 216)
+pygame.init()
+clock = pygame.time.Clock()
+rectangles = [
+    { "rect": { "x": 50, "y": 100, "width": 250, "height": 50 }, "color": RED },
+    { "rect": { "x": 50, "y": 200, "width": 100, "height": 200 }, "color": DARK_GRAY },
+    { "rect": { "x": 200, "y": 200, "width": 100, "height": 100 }, "color": BLUE },
+    { "rect": { "x": 200, "y": 350, "width": 400, "height": 50 }, "color": MAGENTA },
+    { "rect": { "x": 350, "y": 100, "width": 50, "height": 200 }, "color": PASTEL_BLUE },
+    { "rect": { "x": 450, "y": 100, "width": 150, "height": 100 }, "color": GREEN },
+    { "rect": { "x": 450, "y": 250, "width": 150, "height": 50 }, "color": PASTEL_PINK }
+]
+# Definir la finestra
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('Window Title')
+
+
+# Definir les dades
+# Bucle de l'aplicació
+def main():
+    is_looping = True
+    #global color
+    while is_looping:
+        is_looping = app_events()
+        app_run()
+        app_draw()
+
+        clock.tick(60) # Limitar a 60 FPS
+
+    # Fora del bucle, tancar l'aplicació
+    pygame.quit()
+    sys.exit()
+
+# Gestionar events
+def app_events():
+    global dragging, clicked
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # Botó tancar finestra
+            return False
+    return True
+
+# Fer càlculs
+def app_run():
+    pass
+
+        
+        
+# Dibuixar
+def app_draw():
+    global color
+    # Pintar el fons de blanc
+    screen.fill(WHITE)
+
+    # Dibuixar la graella
+    utils1.draw_grid(pygame, screen, 50)
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+
+    for i in rectangles:
+        rect = pygame.Rect(i["rect"]["x"], i["rect"]["y"],i["rect"]["width"],i["rect"]["height"])
+        color = BLACK
+        square_x = i["rect"]["x"]
+        square_y = i["rect"]["y"]
+        width = i["rect"]["width"]
+        height = i["rect"]["height"]
+        if square_x <= mouse_x <= square_x + width and square_y <= mouse_y <= square_y + height:
+            color = i["color"]
+
+        pygame.draw.rect(screen, color,rect)
+
+    # Actualitzar el dibuix a la finestra
+    pygame.display.update()
+
+if __name__ == "__main__":
+    main()
---
+++ exercici0011.py
@@ -0,0 +1,119 @@
+import math
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+import sys
+import utils1
+import utils
+
+# Definir colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (50, 120, 200)
+GREEN = (0,254,32)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+DARK_GRAY = (64, 64, 64)
+GRAY = (128, 128, 128)
+LIGHT_GRAY = (192, 192, 192)
+SOFT_RED = (244, 67, 54)
+SOFT_GREEN = (76, 175, 80)
+SOFT_BLUE = (33, 150, 243)
+SOFT_YELLOW = (255, 235, 59)
+SOFT_ORANGE = (255, 152, 0)
+SOFT_PURPLE = (156, 39, 176)
+PASTEL_PINK = (255, 182, 193)
+PASTEL_GREEN = (119, 221, 119)
+PASTEL_BLUE = (173, 216, 230)
+PASTEL_PURPLE = (216, 191, 216)
+pygame.init()
+clock = pygame.time.Clock()
+
+# Definir la finestra
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('Window Title')
+move_left = move_right = False
+radius = 25
+circle_x = 100
+circle_y = 250
+color = BLACK
+width = 640
+# Definir les dades
+# Bucle de l'aplicació
+def main():
+    is_looping = True
+    #global color
+    while is_looping:
+        is_looping = app_events()
+        app_run()
+        app_draw()
+
+        clock.tick(60) # Limitar a 60 FPS
+
+    # Fora del bucle, tancar l'aplicació
+    pygame.quit()
+    sys.exit()
+
+# Gestionar events
+def app_events():
+    global dragging, clicked, move_left, move_right
+    
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                move_left = True
+            if event.key == pygame.K_RIGHT:
+                move_right = True
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                move_left = False
+            if event.key == pygame.K_RIGHT:
+                move_right = False
+        if event.type == pygame.QUIT: # Botó tancar finestra
+            return False
+    return True
+
+# Fer càlculs
+def app_run():
+    global move_left, move_right, circle_x, circle_y,width,radius
+    delta_time = clock.get_time() / 1000.0
+    speed = 100
+    displacement = speed * delta_time
+
+    if move_right:
+        circle_x = circle_x + displacement
+        if circle_x > width - radius:
+            circle_x = width
+
+    if move_left:
+        circle_x = circle_x - displacement
+        if circle_x < radius:
+            circle_x = radius
+    
+    radius = 10 + (circle_x / 8)
+
+# Dibuixar
+def app_draw():
+    global color
+    # Pintar el fons de blanc
+    screen.fill(WHITE)
+
+    # Dibuixar la graella
+    utils1.draw_grid(pygame, screen, 50)
+
+    pygame.draw.circle(screen, color, (circle_x, circle_y), radius)
+  
+
+    # Actualitzar el dibuix a la finestra
+    pygame.display.update()
+
+if __name__ == "__main__":
+    main()
