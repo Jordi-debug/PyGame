337d07cf07cdc0ab7d57319c644114c06dcdebbc
Jordi-debug <jordifrikigamer@gmail.com>
2024-11-12 11:48:00 +0100

    Ejercicio 3 hecho (un cuadrado que se mueve con las teclas y cambia de color depende de la dirección)

--- Ejercicio propio-3.py
+++ Ejercicio propio-3.py
@@ -9,6 +9,20 @@ import utils
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (50, 120, 200)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+SOFT_RED = (244, 67, 54)
+SOFT_GREEN = (76, 175, 80)
+SOFT_BLUE = (33, 150, 243)
+SOFT_YELLOW = (255, 235, 59)
+SOFT_ORANGE = (255, 152, 0)
+SOFT_PURPLE = (156, 39, 176)
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -16,6 +30,11 @@ clock = pygame.time.Clock()
 # Definir la finestra
 screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption('Window Title')
+color = SOFT_PURPLE
+move_up = move_down = move_left = move_right = False
+square_x = 50
+square_y = 50
+speed = 5
 
 # Definir les dades
 
@@ -37,10 +56,27 @@ def main():
 # Gestionar events
 def app_events():
     global move_left , move_right , move_up , move_down
-    global sprint
-    global color
+
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                move_up = True
+            if event.key == pygame.K_DOWN:
+                move_down = True
+            if event.key == pygame.K_LEFT:
+                move_left = True
+            if event.key == pygame.K_RIGHT:
+                move_right = True
         
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                move_up = False
+            if event.key == pygame.K_DOWN:
+                move_down = False
+            if event.key == pygame.K_LEFT:
+                move_left = False
+            if event.key == pygame.K_RIGHT:
+                move_right = False
     
         if event.type == pygame.QUIT: # Botó tancar finestra
             return False
@@ -48,7 +84,20 @@ def app_events():
 
 # Fer càlculs
 def app_run():
-    pass
+    global color, move_down, move_left, move_right, move_up,square_y, square_x
+    
+    if move_up:
+        square_y -= speed
+        color = CYAN
+    elif move_down:
+        square_y += speed
+        color = SOFT_BLUE
+    elif move_right:
+        square_x += speed
+        color = SOFT_GREEN
+    elif move_left:
+        square_x -= speed
+        color = SOFT_RED
 
 # Dibuixar
 def app_draw():
@@ -59,7 +108,7 @@ def app_draw():
     # Dibuixar la graella
     utils.draw_grid(pygame, screen, 50)
 
-
+    pygame.draw.rect(screen, color, (square_x, square_y,50,50))
 
 
 
